a0c4ba1041340f4cc1beb0a795cf4e3461a9ddf5
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-06-19 21:48:15 -0300

    avanzando

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -14,6 +14,9 @@ import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
   const router = useRouter()
+  const [activeJobType, setActiveJobType] = useState('Full-time')
+
+  const jobTypes = ['Full-time', 'Part-time', 'Contractor']
 
   return (
     <View>
@@ -30,14 +33,32 @@ const Welcome = () => {
             placeholder='What are you looking for?'
           />
         </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity>
-        <Image
-          source={icons.search}
-          resizeMode='contain'
-          style={styles.searchBtnImage}
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}>
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
-      </TouchableOpacity>
+      </View>
     </View>
   )
 }
